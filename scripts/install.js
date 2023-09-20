68a997ae875c4bbdefe5dd10327632cf94a90742
n-devs <n.devs260340@gmail.com>
2023-09-20 15:39:34 +0700

    update

--- scripts/install.js
+++ scripts/install.js
@@ -1,21 +1,21 @@
 const path = require("path");
 const execSync = require("child_process").execSync;
-const {libPackages} =require('../config/libpackPaths')
+const { libPackages } = require('../config/libpackPaths')
 
 function exec(cmd) {
   execSync(cmd, { stdio: "inherit", env: process.env });
 }
 
 const cwd = process.cwd();
-
+console.log(libPackages);
 const config = require(`${libPackages}`)
 
-if(config.libs.lengthl > 0 ) {
+if (config.libs.lengthl > 0) {
   config.libs.forEach(packageName => {
     process.chdir(path.resolve(__dirname, "../libs/" + packageName));
     exec("npm i");
   });
-}else {
+} else {
   console.error("! error not foud libs in libpacks config file");
 }
 
